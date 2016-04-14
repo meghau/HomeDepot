8eeafc14166f9827f51f2942aa6d1cc8f0d339c3
Akanksha Chandre <achand10@ncsu.edu>
2016-04-13 21:54:25 -0400

    clean data file latest

--- cleaning.py
+++ cleaning.py
@@ -2,7 +2,7 @@ import pandas
 from nltk import*
 from nltk.corpus import wordnet
 import re
-import text2num
+#import text2num
 
 #download("stopwords")
 def word_tag(tag):
@@ -21,32 +21,38 @@ def word_tag(tag):
 def clean_entry(entry):
 	entry = entry.lower()
 	entry = word_tokenize(entry)
-	tokens_pos = pos_tag(entry)
-	entry = list()
-	for token in tokens_pos:
-		tag = word_tag(token[1])
-		if tag!='':
-			entry.append(lemmatizer.lemmatize(token[0],tag))
+	list_words = list()
+	for word in entry:
+		list_words.append(stemmer.stem(word))
+	entry = ' '.join(list_words)
+	entry = re.sub("[^\w]", " ",  entry).split()
 	entry = ' '.join(entry)
-	entry_wordList = re.sub("[^\w]", " ",  entry).split()
-	entry = ' '.join((set(entry_wordList) - stopwords))
-	print entry
 	return entry
 
 
 def main():
-	global prd_entry,stopwords,lemmatizer
+	global prd_entry,stopwords,lemmatizer,stemmer
 	data = pandas.read_csv('data.csv')
 	stopwords = set(corpus.stopwords.words('english'))
-	lemmatizer = WordNetLemmatizer()
-	data_subset = data[['product_uid','product_title','product_description']]
-	data_subset = data_subset.drop_duplicates()
-	data_subset['product_title'] = data_subset['product_title'].map(lambda x: set(word_tokenize(x)) - stopwords).map(lambda l:''.join(l))
-	print data_subset['product_title'][0]
+	stemmer = SnowballStemmer('english')
+	#data_subset = data[['product_uid','product_title','product_description']]
+	#data_subset = data_subset.drop_duplicates()
+	data['product_title'] = data['product_title'].map(lambda x:clean_entry(x))
+	data['product_title'] = data['product_title'].map(lambda x: set(word_tokenize(x)) - stopwords).map(lambda l:' '.join(l))
+
+	data['product_description'] = data['product_description'].map(lambda x:clean_entry(x))
+	data['product_description'] = data['product_description'].map(lambda x: set(word_tokenize(x)) - stopwords).map(lambda l:' '.join(l))
+
+	data['search_term'] = data['search_term'].map(lambda x:clean_entry(x))
+	data['search_term'] = data['search_term'].map(lambda x: set(word_tokenize(x)) - stopwords).map(lambda l:' '.join(l))
+
+	print data['product_title'][0]
+	print data['product_description'][0]
+	print data['search_term'][0]
 
 	#data_subset['product_title'].apply(clean_entry)
 	#data_subset['product_description'].apply(clean_entry)
-	#data_subset.to_csv('clean_data.csv')
+	data.to_csv('clean_data.csv')
 	
 
 
