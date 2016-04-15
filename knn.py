f3efb15f1efb83a510c9fb5c8ddf418be7fdbf95
priyanka <pspurani@ncsu.edu>
2016-04-15 00:39:27 -0700

    k-nearest-neighbor

---
+++ knn.py
@@ -0,0 +1,37 @@
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor, BaggingRegressor, AdaBoostRegressor
+
+train = pd.read_csv('data_train.csv')
+test = pd.read_csv('data_test.csv')
+
+y_train = train['relevance'].values
+X_train = train.drop(['id','relevance'],axis=1).values
+y_test = test['relevance'].values
+X_test = test.drop(['id','relevance'],axis=1).values
+
+# K-Nearest Neighbor
+
+knr=KNeighborsRegressor(n_neighbors=500)
+#0.5269
+
+knr.fit(X_train, y_train)
+y_pred = knr.predict(X_test)
+
+# confusion_matrix(y_test, y_pred)
+rms = sqrt(mean_squared_error(y_test, y_pred))
+print rms
+
+
+
+
+
+
+
+
+
+
+
