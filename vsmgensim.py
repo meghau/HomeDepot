1e2d074f3320586e14a5720a7a5640e79ab7fae3
Sagar Manohar <sagarmanohar24@gmail.com>
2016-04-19 02:32:44 -0400

    Added LSI feature

--- vsmgensim.py
+++ vsmgensim.py
@@ -19,22 +19,31 @@ class MyCorpus(object):
 dictionary = corpora.Dictionary(line.lower().split() for line in desc)
 corpus = MyCorpus()
 tfidf = models.TfidfModel(corpus)
-
+lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=50)
+#import code;code.interact(local=locals())
 query = [str(q).split(" ") for q in term]
 desc = [d.split(" ") for d in desc]
 
+
+
 S = []
+L = []
 for i in range(len(desc)):
 	S.append(similarities.MatrixSimilarity([tfidf[dictionary.doc2bow(desc[i])]],num_features=len(dictionary))[tfidf[dictionary.doc2bow(query[i])]])
+	L.append(similarities.MatrixSimilarity([lsi[tfidf[dictionary.doc2bow(desc[i])]]],num_features=len(dictionary))[lsi[tfidf[dictionary.doc2bow(query[i])]]])
 
-X = [s[0] for s in S]
-w = pd.Series(X)
-w.to_csv('vsm.csv')
-X = np.asarray(X)
-X = X.reshape((len(X), 1))
+#import code;code.interact(local=locals())
+A = [s[0] for s in S]
+B = [l[0] for l in L]
+d = pd.DataFrame({'S':pd.Series(A), 'L':pd.Series(B)})
+d.to_csv('vsmlsi.csv',header=False)
+A = np.asarray(A)
+B = np.asarray(B)
+
+X = np.column_stack((A, B))
+#X = X.reshape((len(X), 1))
 model = SGDRegressor()
 model.fit(X, Y)
-
 y_pred = model.predict(X)
 rms = sqrt(mean_squared_error(Y, y_pred))
 print rms
